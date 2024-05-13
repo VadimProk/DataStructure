171064a02846fd3c8964d883ede87158469e168a
VadimProk <provad40@gmail.com>
2024-05-13 09:25:05 +0300

    Add functionality to SinglyLinkedList: access element by index and add/remove element by index

--- DataStructure/SinglyLinkedList.h
+++ DataStructure/SinglyLinkedList.h
@@ -54,5 +54,56 @@ public:
         current->next.reset();
         size--;
     }
+
+    T operator[](int index) {
+        if (index < 0 || index > size - 1) throw std::out_of_range("Index is out of range");
+
+        single_node::Node<T>* current = head.get();
+        for (int i = 1; i <= index; i++) {
+            current = current->next.get();
+        }
+        return current->data;
+    }
+
+    void insertAtIndex(T data, int index) {
+        if (index == 0) {
+            insertAtBeginning(data);
+            return;
+        }
+
+        if (index == size) {
+            insertAtEnd(data);
+            return;
+        }
+
+        if (index < 0 || index > size - 1) throw std::out_of_range("Індекс виходить за межі діапазону");
+
+        std::unique_ptr<single_node::Node<T>> newNode = std::make_unique<single_node::Node<T>>(data);
+        single_node::Node<T>* previous = head.get();
+        for (int i = 1; i <= index - 1; i++) {
+            previous = previous->next.get();
+        }
+        newNode->next = std::move(previous->next);
+        previous->next = std::move(newNode);
+        size++;
+    }
+
+    void removeAtIndex(int index) {
+        if (index < 0 || index > size - 1) throw std::out_of_range("Індекс виходить за межі діапазону");
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        if (index == size - 1) {
+            removeLast();
+            return;
+        }
+        single_node::Node<T>* previous = head.get();
+        for (int i = 1; i <= index - 1; i++) {
+            previous = previous->next.get();
+        }
+        previous->next = std::move(previous->next->next);
+        size--;
+    }
 };
 
