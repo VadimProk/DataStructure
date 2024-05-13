ad41fce906a148bfee40a60ca0711e4273b9d1d5
VadimProk <provad40@gmail.com>
2024-05-13 09:09:27 +0300

    add functionality to SinglyLinkedList: append to the beginning/end of the list

--- DataStructure/SinglyLinkedList.h
+++ DataStructure/SinglyLinkedList.h
@@ -10,5 +10,27 @@ private:
 public:
 	SinglyLinkedList() : head{ nullptr }, size{ 0 } {}
 	SinglyLinkedList(T data) : head{std::make_unique<single_node::Node<T>>(data) }, size{ 1 } {}
+
+    void insertAtBeginning(T data) {
+        std::unique_ptr<single_node::Node<T>> newNode = std::make_unique<single_node::Node<T>>(data);
+        newNode->next = std::move(head);
+        head = std::move(newNode);
+        size++;
+    }
+
+    void insertAtEnd(T data) {
+        std::unique_ptr<single_node::Node<T>> newNode = std::make_unique<single_node::Node<T>>(data);
+        single_node::Node<T>* current = head.get();
+        if (!current) {
+            head = std::move(newNode);
+            size++;
+            return;
+        }
+        while (current->next) {
+            current = current->next.get();
+        }
+        current->next = std::move(newNode);
+        size++;
+    }
 };
 
