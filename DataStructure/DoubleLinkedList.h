82c420f3a2513b5a66b19303b9532213272c9cb3
VadimProk <provad40@gmail.com>
2024-05-13 09:33:39 +0300

    add functionality to DoubleLinkedList: append to the beginning/end of the list

--- DataStructure/DoubleLinkedList.h
+++ DataStructure/DoubleLinkedList.h
@@ -11,5 +11,33 @@ private:
 public:
 	DoubleLinkedList() : head{ nullptr }, tail{ nullptr }, size{ 0 } {}
 	DoubleLinkedList(T data) : head{ std::make_shared<double_node::Node<T>>(data) }, tail{ head }, size{ 1 } {}
+
+    void insertAtBeginning(T data) {
+        std::shared_ptr<double_node::Node<T>> newNode = std::make_shared<double_node::Node<T>>(data);
+
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    void insertAtEnd(T data) {
+        std::shared_ptr<double_node::Node<T>> newNode = std::make_shared<double_node::Node<T>>(data);
+
+        if (!tail) {
+            head = tail = newNode;
+        }
+        else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
 };
 
